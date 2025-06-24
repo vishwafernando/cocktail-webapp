46353c643ac0fc85d5fef1d7992453dc21dbc1d1
Vishwa Fernando <fernandovishwa66@gmail.com>
2025-06-24 22:27:04 +0530

    Update Cocktails.jsx

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -24,8 +24,8 @@ const Cocktails = () => {
 
     return (
         <section id="cocktails" className="noisy">
-            <img src="/images/cocktail-left-leaf.png" loading="lazy" alt="l-leaf" id="c-left-leaf" />
-            <img src="/images/cocktail-right-leaf.png" loading="lazy" alt="r-leaf" id="c-right-leaf" />
+            <img src="/images/cocktail-left-leaf.png"  alt="l-leaf" id="c-left-leaf" />
+            <img src="/images/cocktail-right-leaf.png"  alt="r-leaf" id="c-right-leaf" />
 
             <div className="list">
                 <div className="popular">
